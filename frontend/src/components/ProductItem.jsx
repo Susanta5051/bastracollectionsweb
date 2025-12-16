af46950d5230ac5a079a17e2f490c0828169f371
Susanta Das <dassushant590@gmail.com>
2025-12-16 23:23:56 +0530

    Add height to product image in ProductItem component

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -11,7 +11,7 @@ const ProductItem = ( {id,image,name,price}) => {
     <div>
         <Link className='text-grey-700 cursor-pointer' to={`/product/${id}`}>
         <div className='overflow-hidden'>
-            <img src={image[0]} alt='name' className='hover:scale-110 transition ease-in-out' />  
+            <img src={image[0]} alt='name' className='hover:scale-110 h-70 transition ease-in-out' />  
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency} {price}.00</p>
